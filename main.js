c35ff82b466ea9d65e982061c68b9df957a88b3d
rlawlsdnrqnq <83013198+rlawlsdnrqnq@users.noreply.github.com>
2021-07-14 16:38:38 +0900

    v2

--- main.js
+++ main.js
@@ -2,21 +2,24 @@ const picture = document.querySelector(".picture");
 const emptys = document.querySelectorAll(".empty");
 
 picture.addEventListener("dragstart", dragStart);
+picture.addEventListener("dragend", dragEnd);
 emptys.forEach((empty) => {
   empty.addEventListener("dragenter", dragEnter);
   empty.addEventListener("dragover", dragOver);
   empty.addEventListener("dragleave", dragLeave);
   empty.addEventListener("drop", drop);
 });
-//drag cursor
 
 //drag function
-
 function dragStart(e) {
   e.dataTransfer.setData("text/plain", e.target.id);
   setTimeout(() => {
-    e.target.classList.add("invisible");
+    e.target.classList.add("hide");
   }, 0);
+  picture.classList.add("hold");
+}
+function dragEnd(e) {
+  picture.classList.remove("hold");
 }
 function dragOver(e) {
   e.preventDefault();
@@ -34,5 +37,5 @@ function drop(e) {
   const id = e.dataTransfer.getData("text/plain");
   const draggable = document.getElementById(id);
   e.target.append(draggable);
-  draggable.classList.remove("invisible");
+  draggable.classList.remove("hide");
 }
